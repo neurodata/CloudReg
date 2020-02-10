68d8e51c3fa41670a8f9f6d0f47ad688dd40f838
Vikram Chandrashekhar <vikramc@jhmi.edu>
2020-02-10 12:52:32 -0500

    updated ingest

--- scripts/ingest_image_stack.py
+++ scripts/ingest_image_stack.py
@@ -11,8 +11,10 @@ from psutil import virtual_memory
 import joblib
 from concurrent.futures import ProcessPoolExecutor
 
+
 def get_vol_at_mip(precomputed_path, mip, parallel=False):
-    return CloudVolume(precomputed_path,mip=mip,parallel=parallel)
+    return CloudVolume(precomputed_path, mip=mip, parallel=parallel)
+
 
 def create_cloud_volume(precomputed_path,img_size,voxel_size, num_hierarchy_levels=5):
     info = CloudVolume.create_new_info(
@@ -27,56 +29,58 @@ def create_cloud_volume(precomputed_path,img_size,voxel_size, num_hierarchy_leve
         chunk_size      = [ 1024, 1024, 1 ], # units are voxels
         volume_size     = img_size, # e.g. a cubic millimeter dataset
     )
-    vol = CloudVolume(precomputed_path,info=info)
+    vol = CloudVolume(precomputed_path, info=info)
     # add mip 1
-    [vol.add_scale((2**i,2**i,1)) for i in range(num_hierarchy_levels)]
+    [vol.add_scale((2**i, 2**i, 1)) for i in range(num_hierarchy_levels)]
     vol.commit_info()
     return vol
+
 
 def process(z, img):
     start = time.time()
     global layer_path, num_mips
-    vols = [get_vol_at_mip(layer_path,i,parallel=False) for i in range(num_mips)]
+    vols = [get_vol_at_mip(layer_path, i, parallel=False) for i in range(num_mips)]
     if img.dtype in (np.uint8, np.uint16, np.float32, np.float64):
         img_pyramid = tinybrain.accelerated.average_pooling_2x2(img, num_mips=num_mips)
     else:
         img_pyramid = tinybrain.accelerated.mode_pooling_2x2(img, num_mips=num_mips)
-    vols[0][:,:, z] = img
+    vols[0][:, :, z] = img
     for i in range(num_mips-1):
         vols[i+1][:,:,z] = img_pyramid[i]
 
-    print(f'Processing {z} took {time.time() - start}')
+    print(f"Processing {z} took {time.time() - start}")
+
 
 def main():
 
-    parser = argparse.ArgumentParser(description='Ingest a tif stack into S3.')
+    parser = argparse.ArgumentParser(description='Ingest an image stack into S3.')
     parser.add_argument('-s3_path', help='S3 path to store image as precomputed volume. ', type=str)
     parser.add_argument('-img_stack', help='Path to image stack to be uploaded', type=str)
     parser.add_argument('-fmt', help='extension of file. can be tif or img', type=str)
     parser.add_argument('-voxel_size', help='Voxel size of image. 3 numbers in nanometers', nargs=3, type=float)
     parser.add_argument('--dtype', help='Datatype of image', type=str, default='uint16')
-    parser.add_argument('--new_channel', help='True or False depending on if this is an existing channel or new channel needs to be created', default=False)
-    
+
     args = parser.parse_args()
 
-    if (args.fmt == 'tif'): img = tf.imread(os.path.expanduser(args.img_stack))
-    else: 
+    if (args.fmt == 'tif'): 
+        img = tf.imread(os.path.expanduser(args.img_stack))
+    else:
         tmp = sitk.ReadImage(os.path.expanduser(args.img_stack))
 #        tmp = sitk.RescaleIntensity(tmp, outputMinimum=0, outputMaximum=10000)
         img = sitk.GetArrayFromImage(tmp)
     img = np.asarray(img, dtype=args.dtype)
-    
+
     vol = create_cloud_volume(args.s3_path, img.shape, args.voxel_size)
 
-    mem = virtual_memory() 
-    num_procs = min(math.floor(mem.total/(img.shape[0]*img.shape[1]*8)),joblib.cpu_count())
+    mem = virtual_memory()
+    num_procs = min(math.floor(mem.total/(img.shape[0]*img.shape[1]*8)), joblib.cpu_count())
     print(f"num processes: {num_procs}")
     print(f"layer path: {vol.layer_cloudpath}")
     global layer_path, num_mips
     num_mips = 7
     layer_path = vol.layer_cloudpath
 
-    data = [(i,img[:,:,i]) for i in range(img.shape[-1])]
+    data = [(i, img[:, :, i]) for i in range(img.shape[-1])]
     files = [i[1] for i in data]
     zs = [i[0] for i in data]
 
