dfd6935e4157dddd58ceba75083f60c577215901
Ubuntu <ubuntu@ip-10-0-230-115.ec2.internal>
2020-02-11 16:51:20 +0000

    updates to ingest

--- scripts/ingest_image_stack.py
+++ scripts/ingest_image_stack.py
@@ -16,22 +16,27 @@ def get_vol_at_mip(precomputed_path, mip, parallel=False):
     return CloudVolume(precomputed_path, mip=mip, parallel=parallel)
 
 
-def create_cloud_volume(precomputed_path,img_size,voxel_size, num_hierarchy_levels=5):
+def create_cloud_volume(precomputed_path,img_size,voxel_size, num_hierarchy_levels=5, parallel=True):
+    global args
+    if args.dtype == 'uint64':
+        layer_type = 'segmentation'
+    else:
+        layer_type = 'image'
     info = CloudVolume.create_new_info(
         num_channels    = 1,
-        layer_type      = 'image',
-        data_type       = 'uint16', # Channel images might be 'uint8'
+        layer_type      = layer_type,
+        data_type       = args.dtype, # Channel images might be 'uint8'
         encoding        = 'raw', # raw, jpeg, compressed_segmentation, fpzip, kempressed
         resolution      = voxel_size, # Voxel scaling, units are in nanometers
         voxel_offset    = [0, 0, 0], # x,y,z offset in voxels from the origin
         # Pick a convenient size for your underlying chunk representation
         # Powers of two are recommended, doesn't need to cover image exactly
-        chunk_size      = [ 1024, 1024, 1 ], # units are voxels
+        chunk_size      = [ 512, 512, 1 ], # units are voxels
         volume_size     = img_size, # e.g. a cubic millimeter dataset
     )
-    vol = CloudVolume(precomputed_path, info=info)
+    vol = CloudVolume(precomputed_path, info=info, parallel=parallel)
     # add mip 1
-    [vol.add_scale((2**i, 2**i, 1)) for i in range(num_hierarchy_levels)]
+    [vol.add_scale((2**i, 2**i, 1), chunk_size=[512, 512, 1]) for i in range(num_hierarchy_levels)]
     vol.commit_info()
     return vol
 
@@ -44,9 +49,9 @@ def process(z, img):
         img_pyramid = tinybrain.accelerated.average_pooling_2x2(img, num_mips=num_mips)
     else:
         img_pyramid = tinybrain.accelerated.mode_pooling_2x2(img, num_mips=num_mips)
-    vols[0][:, :, z] = img
+    vols[0][:, :, z] = img[:,:,None]
     for i in range(num_mips-1):
-        vols[i+1][:,:,z] = img_pyramid[i]
+        vols[i+1][:,:,z] = img_pyramid[i][:,:,None]
 
     print(f"Processing {z} took {time.time() - start}")
 
@@ -60,6 +65,7 @@ def main():
     parser.add_argument('-voxel_size', help='Voxel size of image. 3 numbers in nanometers', nargs=3, type=float)
     parser.add_argument('--dtype', help='Datatype of image', type=str, default='uint16')
 
+    global args
     args = parser.parse_args()
 
     if (args.fmt == 'tif'): 
@@ -70,17 +76,20 @@ def main():
         img = sitk.GetArrayFromImage(tmp)
     img = np.asarray(img, dtype=args.dtype)
 
-    vol = create_cloud_volume(args.s3_path, img.shape, args.voxel_size)
+    img_size = img.shape[::-1]
+    vol = create_cloud_volume(args.s3_path, img_size, args.voxel_size)
+#    vol[:,:,:] = img
+
 
     mem = virtual_memory()
     num_procs = min(math.floor(mem.total/(img.shape[0]*img.shape[1]*8)), joblib.cpu_count())
     print(f"num processes: {num_procs}")
     print(f"layer path: {vol.layer_cloudpath}")
     global layer_path, num_mips
-    num_mips = 7
+    num_mips = 3
     layer_path = vol.layer_cloudpath
 
-    data = [(i, img[:, :, i]) for i in range(img.shape[-1])]
+    data = [(i, img.T[:, :, i]) for i in range(img.shape[0])]
     files = [i[1] for i in data]
     zs = [i[0] for i in data]
 
