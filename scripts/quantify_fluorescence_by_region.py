b3fb98c48888588221f4333b7542f60374d1e57c
Vikram Chandrashekhar <vikramc@jhmi.edu>
2020-04-14 13:30:26 -0700

    adding saving csv to quantification

--- scripts/quantify_fluorescence_by_region.py
+++ scripts/quantify_fluorescence_by_region.py
@@ -3,9 +3,11 @@ import argparse
 from joblib import Parallel, delayed
 import numpy as np
 from cloudvolume import CloudVolume
-from collections import defaultdict,Counter
+from collections import defaultdict, Counter
 from skimage import transform
 from tqdm import tqdm, trange
+import pandas as pd
+from ..ARA_stuff.parse_ara import *
 
 def get_region_stats(atlas_s3_path, data_s3_path, z_slice):
     # create vols
@@ -38,23 +40,54 @@ def combine_results(results):
         total_volume += Counter(i[1])
     return total_fluorescence,total_volume
 
+def get_ara_dict(path_to_ontology):
+    with open('./MBAC/ARA_stuff/ara_ontology.json','r') as fp:
+        f = json.load(fp)
+        tree = build_tree(f)
+        id2name = defaultdict(str)
+        get_child_nodes_from_ontology(tree, id2name)
+    return id2name
+
+def save_results_to_csv(fluorescence_dict,columns,out_path):
+    df = pd.DataFrame.from_dict(fluorescence_dict,orient='index',columns=columns)
+    df.index.name = 'ROI Name'
+    df.sort_values(columns[-1],ascending=False, inplace=True)
+    df.to_csv(out_path)
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('data_s3_path',help='full s3 path to data of interest as precomputed volume. must be of the form `s3://bucket-name/path/to/channel`')
     parser.add_argument('atlas_s3_path',help='full s3 path to transfomed atlas. must have the same number of slices as native resolution data.')
     parser.add_argument('out_path',help='path to save output results')
+    parser.add_argument('--path_to_ontology',help='path to save output results',type=str,defalt='~/MBAC/ARA_stuff/ara_ontology.json')
     parser.add_argument('--num_procs',help='number of processes to use',default=35, type=int)
     args = parser.parse_args()
     data_vol = CloudVolume(args.data_s3_path)
+    id2name = get_ara_dict(args.path_to_ontology)
+    experiment_name = '_'.join(args.data_s3_path.split('/')[-2:])
+
     results = Parallel(args.num_procs)(delayed(get_region_stats)(args.atlas_s3_path, args.data_s3_path,i) 
                        for i in trange(data_vol.scales[0]['size'][-1]))
     total_fluorescence, total_volume = combine_results(results)
     fluorescence_density = defaultdict(float)
     for i,j in total_fluorescence.items():
         fluorescence_density[i] = float(j)/float(total_volume[i])
-    experiment_name = '_'.join(args.data_s3_path.split('/')[-2:])
-    with open(f'{args.out_path}/{experiment_name}_fluorescence_quantification', 'wb') as fp:
-        pickle.dump([total_fluorescence,total_volume], fp)
-
+    fluorescence_density_sorted = {k: v for k, v in sorted(fluorescence_density.items(), key=lambda item: item[1])}
+    # density by roi
+    columns = ['atlas id','fluorescence density']
+    outfile = f'{args.out_path}/{experiment_name}_'
+    fluorescence_density_roi = defaultdict(float)
+    for i,j in fluorescence_density_sorted.items():
+        fluorescence_density_roi[id2name[i]] = [i,j]
+    save_results_to_csv(total_fluorescence_roi, columns, outfile + 'fluorescence_density')
+    
+    # total fluorescence by roi
+    columns = ['atlas id','total fluorescence']
+    total_fluorescence_roi = defaultdict(float)
+    for i,j in total_fluorescence.items():
+        total_fluorescence_roi[id2name[i]] = [i,j]
+    total_fluorescence_roi = {k: v for k, v in sorted(total_fluorescence_roi.items(), key=lambda item: item[1])}
+    save_results_to_csv(total_fluorescence_roi, columns, outfile + 'total_fluorescence')
+    
 if __name__ == "__main__":
     main()
