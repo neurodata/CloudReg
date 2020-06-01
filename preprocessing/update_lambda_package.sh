d5730b75cf0077e5d1af68e3b435b1269740400e
Vikram Chandrashekhar <vikramc@jhmi.edu>
2020-06-01 15:20:00 -0700

    removed unused files

--- preprocessing/update_lambda_package.sh
+++
@@ -1,5 +0,0 @@
-cd bias_correction_lambda/lib/python3.8/site-packages
-zip -r9 ~/MBAC/preprocessing/lambda_function.zip .
-cd ~/MBAC/preprocessing/
-zip -g lambda_function.zip lambda_correct_tile.py
-aws lambda update-function-code --function-name colm_tile_correction --zip-file fileb://lambda_function.zip
