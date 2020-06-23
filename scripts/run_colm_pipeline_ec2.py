a3eb49d963b26cc0cdc771553360b2004f2dd595
Vikram Chandrashekhar <vikramc@jhmi.edu>
2020-06-23 12:40:16 -0700

    bug fix

--- scripts/run_colm_pipeline_ec2.py
+++ scripts/run_colm_pipeline_ec2.py
@@ -63,7 +63,7 @@ def run_colm_pipeline(
 
     # now run command on instance
     # update the code on the instance
-    update_command = 'cd CloudReg; git pull;'
+    update_command = 'mkdir -p ~/ssd1 ~/ssd2; cd CloudReg; git pull;'
     errors_update = run_command_on_server(update_command, ssh_key_path, instance.public_ip_address)
     # mount ssds command
     command1 = 'sudo bash CloudReg/scripts/mount_combined_ssds.sh'
