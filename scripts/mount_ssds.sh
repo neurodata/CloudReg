e9f680257dc043b3732b13dfbaff54da0dab8e13
Vikram Chandrashekhar <vikramc@jhmi.edu>
2020-06-23 15:24:50 -0700

    removed unnecessary file

--- scripts/mount_ssds.sh
+++
@@ -1,7 +0,0 @@
-findmnt /dev/nvme0n1
-mkfs.ext4 -E nodiscard -m0 /dev/nvme0n1
-mkfs.ext4 -E nodiscard -m0 /dev/nvme1n1
-mount -o discard /dev/nvme0n1 /home/ubuntu/ssd1
-mount -o discard /dev/nvme1n1 /home/ubuntu/ssd2
-chown ubuntu:ubuntu /home/ubuntu/ssd1
-chown ubuntu:ubuntu /home/ubuntu/ssd2
