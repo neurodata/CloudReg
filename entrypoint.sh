6ad5b8cd81f89f18d67d5c6437a55cccb2aa98be
Vikram Chandrashekhar <vikramc@jhmi.edu>
2020-08-26 11:51:38 -0700

    fix relative import issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ mkdir -p ~/.cloudvolume/secrets/
 # copy over cloudvolume secret
 cp ${CV_CRED_PATH} ~/.cloudvolume/secrets/aws-secret.json
 
-python CloudReg/scripts/colm_pipeline.py $@
+python -m CloudReg/scripts/colm_pipeline.py $@
