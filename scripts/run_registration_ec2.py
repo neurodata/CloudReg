d562ed610cb91127be4ad82fc8dd2a72a8ee3a03
Vikram Chandrashekhar <vikramc@jhmi.edu>
2020-09-19 22:48:24 -0700

    big fix reg

--- scripts/run_registration_ec2.py
+++ scripts/run_registration_ec2.py
@@ -9,7 +9,7 @@ from registration import get_affine_matrix
 import argparse
 import boto3
 
-# python_path = "~/colm_pipeline_env/bin/python3"
+#python_path = "~/colm_pipeline_env/bin/python3"
 python_path = "python3"
 
 
@@ -91,7 +91,7 @@ def run_registration(
     # matlab registration command
     fixed_scale_string = ' '.join([f'{i}' for i in fixed_scale])
     print(fixed_scale_string)
-    command2 = f"time {python_path} CloudReg/scripts/registration.py -input_s3_path {input_s3_path} --output_s3_path {output_s3_path} -orientation {orientation} --rotation {' '.join(map(str,initial_rotation))} --translation {' '.join(map(str,initial_translation))} --scale {fixed_scale_string} -log_s3_path {log_s3_path} --missing_data_correction {missing_data_correction} --grid_correction {grid_correction} --bias_correction {bias_correction} --regularization {sigma_regularization} --iterations {num_iterations}"
+    command2 = f"time {python_path} CloudReg/scripts/registration.py -input_s3_path {input_s3_path} --output_s3_path {output_s3_path} -orientation {orientation} --rotation {' '.join(map(str,initial_rotation))} --translation {' '.join(map(str,initial_translation))} --fixed_scale {fixed_scale_string} -log_s3_path {log_s3_path} --missing_data_correction {missing_data_correction} --grid_correction {grid_correction} --bias_correction {bias_correction} --regularization {sigma_regularization} --iterations {num_iterations}"
     print(command2)
     errors2 = run_command_on_server(command2, ssh_key_path, public_ip_address)
     print(f"errors: {errors2}")
