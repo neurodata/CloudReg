a7d7b6a129782aaf174e2cdf0fdf32000ae16911
Vikram Chandrashekhar <vikramc@jhmi.edu>
2020-09-25 10:19:01 -0700

    updated mounting ssds

--- scripts/mount_combined_ssds.sh
+++ scripts/mount_combined_ssds.sh
@@ -1,10 +1,10 @@
 mkdir -p ~/ssd1 ~/ssd2
 if [ $(($(lsblk | grep nvme | wc -l) - 2)) -lt $((4)) ]
 then
-    mkfs.ext4 -E nodiscard -m0 /dev/nvme0n1
     mkfs.ext4 -E nodiscard -m0 /dev/nvme1n1
-    mount -o discard /dev/nvme0n1 /home/ubuntu/ssd1
-    mount -o discard /dev/nvme1n1 /home/ubuntu/ssd2
+    mkfs.ext4 -E nodiscard -m0 /dev/nvme2n1
+    mount -o discard /dev/nvme1n1 /home/ubuntu/ssd1
+    mount -o discard /dev/nvme2n1 /home/ubuntu/ssd2
     chown ubuntu:ubuntu /home/ubuntu/ssd1
     chown ubuntu:ubuntu /home/ubuntu/ssd2
 
