f4a8abda6370b4d9daf61f628444be33d4391d81
Vikram Chandrashekhar <vikramc@jhmi.edu>
2020-10-12 21:40:35 -0700

    fixed mounting ssds to be name agnostic

--- scripts/mount_combined_ssds.sh
+++ scripts/mount_combined_ssds.sh
@@ -1,16 +1,40 @@
 mkdir -p ~/ssd1 ~/ssd2
 if [ $(($(lsblk | grep nvme | wc -l) - 2)) -lt $((4)) ]
 then
-    mkfs.ext4 -E nodiscard -m0 /dev/nvme1n1
-    mkfs.ext4 -E nodiscard -m0 /dev/nvme2n1
-    mount -o discard /dev/nvme1n1 /home/ubuntu/ssd1
-    mount -o discard /dev/nvme2n1 /home/ubuntu/ssd2
+    for blkdev in $(nvme list | awk '/^\/dev/ { print $1 }'); do
+        i=1
+        mapping=$(nvme id-ctrl --raw-binary "${blkdev}" | grep Instance)
+        if [[ ${mapping} ]]; then
+            echo "$blkdev is $mapping formatting and mounting..."
+            mkfs.ext4 -E nodiscard -m0 ${blkdev}
+            mount -o discard ${blkdev} /home/ubuntu/ssd{i}
+            i++
+        else
+            echo "detected unknown drive letter $blkdev: $mapping. Skipping..."
+        fi
+    done
     chown ubuntu:ubuntu /home/ubuntu/ssd1
-    chown ubuntu:ubuntu /home/ubuntu/ssd2
+    chown ubuntu:ubuntu /home/ubuntu/ssdo2
 
-else 
-    pvcreate /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
-    vgcreate LVMVolGroup /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
+else
+    vgcreate LVMVolGroup 
+    for blkdev in $(nvme list | awk '/^\/dev/ { print $1 }'); do
+        i=0
+        mapping=$(nvme id-ctrl --raw-binary "${blkdev}" | grep Instance)
+        if [[ ${mapping} ]]; then
+            echo "$blkdev is $mapping formatting and mounting..."
+            pvcreate ${blkdev}
+            if [[ ${i} == 0 ]]; then
+                vgcreate LVMVolGroup ${blkdev}
+            else
+                echo ""
+            fi
+            vgextend LVMVolGroup ${blkdev}
+            i++
+        else
+            echo "detected unknown drive letter $blkdev: $mapping. Skipping..."
+        fi
+    done
     lvcreate -l 50%FREE -n vol1 LVMVolGroup
     lvcreate -l 100%FREE -n vol2 LVMVolGroup
     #  create filesystems
