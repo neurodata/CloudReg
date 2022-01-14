6ad878acc42f96257549bc512034235cbc764d52
Thomas Athey <tom.l.athey@gmail.com>
2022-01-14 11:57:25 -0500

    2x2x2 ds

--- cloudreg/scripts/create_precomputed_volume.py
+++ cloudreg/scripts/create_precomputed_volume.py
@@ -94,7 +94,7 @@ def process(z, file_path, layer_path, num_mips):
     # array = load_image(file_path)[..., None]
     # array = tf.imread(file_path).T[..., None]
     array = np.squeeze(np.array(Image.open(file_path))).T[..., None]
-    img_pyramid = tinybrain.accelerated.average_pooling_2x2(array, num_mips)
+    img_pyramid = tinybrain.accelerated.average_pooling_2x2x2(array, num_mips)
     vols[0][:, :, z] = array
     for i in range(num_mips - 1):
         vols[i + 1][:, :, z] = img_pyramid[i]
@@ -120,7 +120,7 @@ def create_precomputed_volume(
 
     img_size = get_image_dims(files)
     # compute num_mips from data size
-    chunk_size = [128, 128, 1]
+    chunk_size = [128, 128, 128]
     num_mips = calc_hierarchy_levels(img_size, lowest_res=chunk_size[0])
     # convert voxel size from um to nm
     vol = create_cloud_volume(
