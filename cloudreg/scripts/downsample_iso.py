0661164becfac9d63718990e763c8c6ad3d275e7
Thomas Athey <tom.l.athey@gmail.com>
2022-02-04 15:44:37 -0500

    iso

--- cloudreg/scripts/downsample_iso.py
+++ cloudreg/scripts/downsample_iso.py
@@ -2,33 +2,50 @@ from taskqueue import LocalTaskQueue
 import igneous.task_creation as tc
 from cloudvolume import CloudVolume
 
-tq = LocalTaskQueue(parallel=8)
+def downsample_isotropically(input_path, output_path):
+  tq = LocalTaskQueue(parallel=8)
 
-src_layer_path = "file:///mnt/data/Neuroglancer_Data/2021_10_06/8557/Ch_647_Iso"
-dest_layer_path = "file:///mnt/data/Neuroglancer_Data/2021_10_06/8557/Ch_647_Iso2"
+  vol = CloudVolume(input_path)
+  print(f"Original chunk size: {vol.chunk_size}, shape: {vol.shape}")
 
-vol = CloudVolume(src_layer_path)
-print(f"chunk size: {vol.chunk_size}, shape: {vol.shape}")
-
-vol = CloudVolume(dest_layer_path, mip=0)
-print(f"chunk size: {vol.chunk_size}, shape: {vol.shape}")
-
-
-# tasks = tc.create_transfer_tasks(
-#   src_layer_path, dest_layer_path, 
-#   chunk_size=[128,128,128], compress=False,
-#   skip_downsamples = True
-# )
-
-# tq.insert(tasks)
-# tq.execute()
-
-tasks = tc.create_downsampling_tasks(
-    dest_layer_path, # e.g. 'gs://bucket/dataset/layer'
-    compress=False, # None, 'gzip', and 'br' (brotli) are options
-    factor=(2,2,2), # common options are (2,2,1) and (2,2,2)
+  tasks = tc.create_transfer_tasks(
+    input_path, output_path, 
+    chunk_size=[128,128,128], compress=False,
+    skip_downsamples = True
   )
 
-tq.insert(tasks)
-tq.execute()
-print("Done!")
+  tq.insert(tasks)
+  tq.execute()
+
+  vol = CloudVolume(output_path, mip=0)
+  print(f"Output chunk size: {vol.chunk_size}, shape: {vol.shape}")
+
+  tasks = tc.create_downsampling_tasks(
+      output_path, # e.g. 'gs://bucket/dataset/layer'
+      compress=False, # None, 'gzip', and 'br' (brotli) are options
+      factor=(2,2,2), # common options are (2,2,1) and (2,2,2)
+    )
+
+  tq.insert(tasks)
+  tq.execute()
+  print("Done!")
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Convert precomputed layer to isotropic downsampled structure. Built to follow create_precomputed_volume"
+    )
+    parser.add_argument(
+        "input_path",
+        help="Path to directory containing layer at highest resolution. e.g. file://<path>",
+    )
+    parser.add_argument(
+        "output_path",
+        help="Path to directory for output volume.",
+    )
+    args = parser.parse_args()
+
+    downsample_isotropically(
+        args.input_path, args.output_path
+    )
