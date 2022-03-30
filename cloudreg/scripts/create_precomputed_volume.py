60766431efef387799cb02075202a00c26ad53cd
Thomas Athey <tom.l.athey@gmail.com>
2022-03-30 12:05:03 -0400

    black

--- cloudreg/scripts/create_precomputed_volume.py
+++ cloudreg/scripts/create_precomputed_volume.py
@@ -168,6 +168,16 @@ def create_precomputed_volume(
 
 
 if __name__ == "__main__":
+    def str2bool(v):
+        if isinstance(v, bool):
+            return v
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')
+
     parser = argparse.ArgumentParser(
         description="Convert local volume into precomputed volume on S3."
     )
@@ -195,11 +205,13 @@ if __name__ == "__main__":
         "--compress",
         help="Whether to use a compressed format for the precomputed volume.",
         default=False,
+        type=str2bool,
     )
     parser.add_argument(
         "--resample_iso",
         help="Whether to immediately write another version of the volume that has isotropic chunks to be able to use several views on neuroglancer.",
         default=False,
+        type=str2bool,
     )
     args = parser.parse_args()
     print(f"comopress: {args.compress}, {type(args.compress)}")
