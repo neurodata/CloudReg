2512da9d8a103c9e38f79994089f64c3e0a8f0e2
Thomas Athey <tom.l.athey@gmail.com>
2022-05-06 16:09:56 -0400

    parent dir option

--- cloudreg/scripts/create_precomputed_volumes.py
+++ cloudreg/scripts/create_precomputed_volumes.py
@@ -19,15 +19,15 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--input_parent_dir",
-        help="Path to parent of directories containing stitched tiles named sequentially.", nargs='+', default=[]
+        help="Path to parent of directories containing stitched tiles named sequentially.", nargs='+'
     )
     parser.add_argument(
         "--s3_output_parent_dir",
-        help="Path to parent of directories containing stitched tiles named sequentially.", nargs='+', default=[]
+        help="Path to parent of directories containing stitched tiles named sequentially.", nargs='+'
     )
     parser.add_argument(
         "--local_output_parent_dir",
-        help="Path to parent of directories containing stitched tiles named sequentially.", nargs='+', default=[]
+        help="Path to parent of directories containing stitched tiles named sequentially.", nargs='+'
     )
     parser.add_argument(
         "--s3_input_paths",
